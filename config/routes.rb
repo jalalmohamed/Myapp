42cf44a38e7cf4f80810902523f03d8b8b570bff
Jalal Mohamed <jalalmohamed@mallow-tech.com>
2022-12-01 15:13:00 +0530

    Initial commit

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  resources :books
+  resources :high_scores
+  get 'greetings/hello'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
